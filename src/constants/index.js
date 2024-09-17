e7d0f03461cc568cda2e3eb887384d99f520830a
Manjushri P Rao <manjushriprao447@gmail.com>
2024-09-17 20:33:57 +0200

    commit

---
+++ src/constants/index.js
@@ -0,0 +1,531 @@
+import {
+  nitk,
+  cluboard,
+  cash_flow,
+  gdsc,
+  iris,
+  ecell,
+  genesis,
+  portfolio,
+  publiclab,
+  zulip,
+  cdc,
+  chargeswap,
+  placeicon,
+  recruitment,
+  huntly,
+  oracle,
+  comicify_ai,
+  greentrust,
+  averlon,
+  devfolio,
+  pba,
+  ethglobal,
+  polkadot,
+  lightspeed,
+  dennisivy,
+  manipal,
+  icon,
+  ethforall
+} from "../assets";
+
+import {
+  AiFillGithub,
+  AiFillInstagram,
+  AiFillLinkedin,
+  AiFillMail,
+  AiOutlineTwitter,
+  AiFillHtml5,
+  AiOutlineGitlab,
+} from "react-icons/ai";
+
+import {
+  SiDjango,
+  SiJavascript,
+  SiBootstrap,
+  SiReact,
+  SiTailwindcss,
+  SiGraphql,
+  SiPython,
+  SiCplusplus,
+  SiC,
+  SiRubyonrails,
+  SiJquery,
+  SiVisualstudiocode,
+  SiPostman,
+  SiGit,
+  SiMysql,
+  SiSolidity,
+  SiNetlify,
+  SiChartdotjs,
+  SiVite,
+  SiArduino,
+  SiWeb3Dotjs,
+  SiIpfs,
+  SiDotnet,
+  SiTwilio,
+  SiFlutter,
+  SiReplit,
+  SiFlask,
+  SiFigma,
+  SiGooglemaps,
+  SiOpenai,
+  SiGooglecloud,
+  SiNextdotjs,
+  SiMicrosoftazure
+} from "react-icons/si";
+
+import {
+  FaHardHat,
+  FaRust
+} from "react-icons/fa";
+
+import {
+  IoIosNotificationsOutline
+} from "react-icons/io";
+
+import {
+  FaGolang
+} from "react-icons/fa6";
+
+import { DiCss3, DiJava, DiMsqlServer, DiRuby } from "react-icons/di";
+
+//export const resumeLink = "https://drive.google.com/file/d/1vkxyMDB5_KpMwt4QXFgT2aqdRizr8Czh/view?usp=sharing";
+//export const repoLink = "https://github.com/mittal-parth/personal-portfolio";
+
+export const callToAction = "https://www.linkedin.com/in/manjushri-prakash-rao";
+
+export const navLinks = [
+  {
+    id: "skills",
+    title: "Skills & Experience",
+  },
+  {
+    id: "education",
+    title: "Education",
+  },
+  {
+    id: "achievements",
+    title: "Achievements",
+  },
+  {
+    id: "projects",
+    title: "Projects",
+  },
+  {
+    id: "openSource",
+    title: "Open Source",
+  },
+  {
+    id: "extraCurricular",
+    title: "Extra Curricular",
+  },
+  {
+    id: "contactMe",
+    title: "Contact Me",
+  },
+];
+
+export const educationList = [
+  {
+    id: "education-1",
+    icon: nitk,
+    title: "BNM Institute of Technology, Bangalore, India",
+    degree: "Bachelor of Technology",
+    duration: "December 2016 - May 2020",
+    content1: "Major: Information Science and Engineering",
+    //content2: "Minor: Information Technology",
+  },
+  /*{
+    id: "education-2",
+    icon: pba,
+    title: "Polkadot Blockchain Academy",
+    degree: "",
+    duration: "May 2024 - June 2024",
+    content1: "Graduated with a distinction in the fifth cohort of the Polkadot Blockchain Academy at the National University of Singapore.",
+  },*/
+];
+/*
+export const achievements = [
+  {
+    id: "a-1",
+    icon: ethglobal,
+    event: "ETHIndia'22 | World's Largest Ethereum Hackathon",
+    position: "Winner",
+    content1: "Top 12 winners among 20k+ registrations",
+    content2: "One of Polygon's Best Public Goods",
+    content3: "Best Module on Biconomy SDK",
+    article: "https://www.thehindu.com/news/cities/Mangalore/nitk-iiit-delhi-team-makes-it-to-top-12-winners-in-ethindia-22/article66238923.ece",
+    project: "https://devfolio.co/projects/chargeswap-3527",
+    youtube: "https://youtu.be/9rieTya8Yds?t=3908",
+  }
+  
+];*/
+
+
+export const skills = [
+  {
+    title: "Programming Languages",
+    items: [
+        {
+            id: "pl-6",
+            icon: DiJava,
+            name: "Java",
+        },
+      {
+        id: "pl-2",
+        icon: SiPython,
+        name: "Python",
+      }
+      
+    ],
+  },
+  {
+    title: "Frameworks/Libraries",
+    items: [
+      {
+        id: "f-1",
+        icon: SiDjango,
+        name: "Django",
+      },
+      {
+        id: "f-2",
+        icon: SiRubyonrails,
+        name: "Ruby on Rails",
+      },
+      {
+        id: "f-3",
+        icon: SiReact,
+        name: "ReactJS",
+      },
+      {
+        id: "f-4",
+        icon: SiBootstrap,
+        name: "Bootstrap",
+      },
+      {
+        id: "f-5",
+        icon: SiTailwindcss,
+        name: "Tailwind CSS",
+      },
+      {
+        id: "f-6",
+        icon: SiJquery,
+        name: "jQuery",
+      },
+      {
+        id: "f-7",
+        icon: SiGraphql,
+        name: "GraphQL",
+      },
+      {
+        id: "f-8",
+        icon: SiDotnet,
+        name: ".NET",
+      },
+    ],
+  },
+  {
+    title: "Tools",
+    items: [
+      {
+        id: "t-1",
+        icon: SiMicrosoftazure,
+        name: "Azure",
+      },
+      {
+        id: "t-2",
+        icon: SiMysql,
+        name: "MySQL",
+      },
+      {
+        id: "t-3",
+        icon: SiPostman,
+        name: "Postman",
+      },
+  
+      {
+        id: "t-5",
+        icon: SiGit,
+        name: "Git",
+      },
+      {
+        id: "t-6",
+        icon: AiFillGithub,
+        name: "GitHub",
+      },
+      {
+        id: "t-7",
+        icon: AiOutlineGitlab,
+        name: "Gitlab",
+      },
+      
+    
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    organisation: "Accenture, India",
+    logo: oracle,
+    link: "https://www.oracle.com/in/",
+    positions: [
+      {
+        title: "DevOps Engineer",
+        duration: "Nov 2020 - Jan 2024",
+        content: [
+          {
+                text: "Cloud Engineer with experience managing Azure environments, supporting business operations through cloud-based solutions, and ensuring the smooth administration of cloud platforms.Adept at maintaining cloud infrastructure, optimizing costs.Proven ability to collaborate with cross - functional teams, engage stakeholders, and implement cloud governance best practices.Skilled in cross - functional team coordination, problem- solving, and continuous improvement initiatives.",
+            link: "",
+          },
+        ],
+      },
+    ],
+  },
+];
+
+export const openSourceContributions = [
+  {
+    id: "os-1",
+    organisation: "PublicLab",
+    logo: publiclab,
+    repo: "plots2",
+    type: "pull-request",
+    status: "merged",
+    title:
+      "Move _wiki to old table look, common template for email and settings digest only",
+    link: "https://github.com/publiclab/plots2/pull/10731",
+    number: "#10731",
+    date: "Feb 24 2022",
+    linesAdded: "101",
+    linesDeleted: "48",
+  },
+  
+  {
+    id: "os-11",
+    organisation: "Zulip",
+    logo: zulip,
+    repo: "zulip",
+    type: "pull-request",
+    status: "merged",
+    title: "invitations: Check invitation from a now-deactivated user.",
+    link: "https://github.com/zulip/zulip/pull/20206",
+    number: "#20206",
+    date: "Dec 30 2021",
+    linesAdded: "31",
+    linesDeleted: "1",
+  },
+  
+];
+
+export const projects = [
+  {
+    id: "project-1",
+    title: "Comicify.ai",
+    github: "https://github.com/ayush4345/Comicify.ai",
+    link: "https://comicify-ai.vercel.app/",
+    image: comicify_ai,
+    content:
+      "Convert any academic/news/boring text into cool comic strips using GPT-3.5 and Stable Diffusion!",
+    stack: [
+      {
+        id: "icon-1",
+        icon: SiReact,
+        name: "React"
+      },
+      {
+        id: "icon-2",
+        icon: SiTailwindcss,
+        name: "TailwindCSS"
+      },
+      {
+        id: "icon-3",
+        icon: SiOpenai,
+        name: "OpenAI"
+      },
+      {
+        id: "icon-4",
+        icon: SiGooglecloud,
+        name: "Google Cloud Platform"
+      },
+      {
+        id: "icon-5",
+        icon: SiFlask,
+        name: "Flask"
+      },
+    ],
+  },
+  {
+    id: "project-2",
+    title: "GreenTrust",
+    github: "https://github.com/mittal-parth/GreenTrust",
+    link: "https://green-trust-fantom.netlify.app/",
+    image: greentrust,
+    content:
+      "Winning project at 3 hackathons, GreenTrust offers a novel solution for obtaining certification in organic farming by organizing credible and decentralized Participatory Guarantee Systems (PGSs).",
+    stack: [
+      {
+        id: "icon-1",
+        icon: SiReact,
+        name: "React"
+      },
+      {
+        id: "icon-2",
+        icon: SiTailwindcss,
+        name: "TailwindCSS"
+      },
+      {
+        id: "icon-3",
+        icon: SiNextdotjs,
+        name: "Next.js"
+      },
+      {
+        id: "icon-4",
+        icon: SiIpfs,
+        name: "IPFS"
+      },
+      {
+        id: "icon-5",
+        icon: SiSolidity,
+        name: "Solidity"
+      },
+      {
+        id: "icon-6",
+        icon: IoIosNotificationsOutline,
+        name: "Push Protocol"
+      },
+    ],
+  },
+  
+ 
+];
+
+export const blogPosts = [
+  {
+    id: "post-1",
+    title: "Blog Post 01 - Title",
+    link: "#",
+    date: new Date().toLocaleDateString(), // Can be edited to any string format
+    image: "https://via.placeholder.com/600/92c952",
+    tags: [
+      {
+        id: "tag-1",
+        name: "tag 01"
+      },
+      {
+        id: "tag-2",
+        name: "tag 03"
+      },
+      {
+        id: "tag-3",
+        name: "tag 03"
+      },
+    ],
+  },
+  {
+    id: "post-2",
+    title: "Blog Post 02 - Title",
+    link: "#",
+    date: new Date().toLocaleDateString(),
+    image: "https://via.placeholder.com/600/d32776",
+    tags: [
+      {
+        id: "tag-1",
+        name: "tag 01"
+      },
+      {
+        id: "tag-2",
+        name: "tag 03"
+      },
+      {
+        id: "tag-3",
+        name: "tag 03"
+      },
+    ],
+  },
+  {
+    id: "post-3",
+    title: "Blog Post 03 - Title",
+    link: "#",
+    date: new Date().toLocaleDateString(),
+    image: "https://via.placeholder.com/600/771796",
+    tags: [
+      {
+        id: "tag-1",
+        name: "tag 01"
+      },
+      {
+        id: "tag-2",
+        name: "tag 03"
+      },
+      {
+        id: "tag-3",
+        name: "tag 03"
+      },
+    ],
+  },
+];
+
+export const stats = [
+  {
+    id: "stats-1",
+    title: "Organisations",
+    value: "2+",
+  },
+  {
+    id: "stats-2",
+    title: "Issues Opened",
+    value: "6+",
+  },
+  {
+    id: "stats-3",
+    title: "Pull Requests",
+    value: "6+",
+  },
+];
+
+export const extraCurricular = [
+  {
+    organisation: "Devfolio",
+    title: "UniDAO Lead",
+    duration: "December 2021 - Present",
+    content: [
+      {
+        text: "Selected among 5 students across the country to lead the initiative and grow the culture of Blockchain and Ethereum, powered by Devfolio.",
+        link: "https://www.linkedin.com/feed/update/urn:li:activity:7097977924686942209/",
+      },
+      {
+        text: "Led a cohort of 37 selected students over 6 weeks to learn and build in the Ethereum ecosystem.",
+        link: "https://www.linkedin.com/feed/update/urn:li:activity:7095310520282480641/",
+      },
+    ],
+    logo: devfolio,
+  },
+ 
+];
+
+export const socialMedia = [
+  {
+    id: "social-media-1",
+    icon: AiFillLinkedin,
+    link: "https://www.linkedin.com/in/manjushri-prakash-rao",
+  },
+  {
+    id: "social-media-2",
+    icon: AiFillGithub,
+      link: "https://github.com/manjushriprao",
+  },
+  {
+    id: "social-media-3",
+    icon: AiFillMail,
+    link: "manjushriprao447@gmail.com",
+  },
+  
+];
+
+export const aboutMe = {
+    name: "Manjushri P Rao",
+    tagLine: "MTS-1 @Oracle | ex-AccentureSoftware Engineer | Automation | Certified in Azure and AWS | Java | python | DevOps | CI/CD | Docker | Kubernetes | GIT | Azure DevOps | Passionate Problem Solver and Team Player | ex-Tech Lead @IRIS,NITK | ETHIndia'22 Winner | NITK'24",
+    intro: "I excel in designing and maintaining robust CI/CD pipelines. I am proficient in automating workflows, managing cloud infrastructure, and ensuring seamless application deployment. Proficient in version control systems like GIT. I have expertise in containerization with Docker and orchestration with Kubernetes. I have a strong background in implementing Agile methodologies for product development. I am highly productive and results-driven in both team and individual tasks, and have a proven ability to learn new technologies quickly.A strong team player, known for my productivity, commitment, and results- driven approach.I quickly learn new technologies and tools, maintain a positive attitude, and possess strong analytical capabilities for problem - solving, I actively contribute to project activities and ensure smooth project delivery."
+}
