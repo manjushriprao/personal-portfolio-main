979f6a68a0eac29fdafba1753a5f2cb9513a0679
manjushriprao <47849201+manjushriprao@users.noreply.github.com>
2024-09-18 16:19:26 +0200

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -59,7 +59,7 @@ const Footer = () => (
     </div>
     <div className="text-center font-poppins font-normal text-dimWhite text-xs sm:text-sm pb-4">
       <p>
-        Made with 💙 by Parth Mittal & the Open Source Community
+        Made with 💙 by Manjushri P Rao & the Open Source Community
       </p>
     </div>
   </footer>
